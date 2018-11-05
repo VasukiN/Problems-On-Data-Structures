0aa4e55a35a2e5c591031c5993a9835f38dd8352
VasukiN <vasuki.hebbar@gmail.com>
2018-11-05 21:53:01 +0530

    SubArrayGivenSUm
    Geeks for geeks - https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0

--- MissingNumberInArray.cs
+++ MissingNumberInArray.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-namespace ConsoleApplication1
-{
-    class MissingNumberInArray
-    {
+//namespace ConsoleApplication1
+//{
+//    class MissingNumberInArray
+//    {
         
-        public static void Main(string[] args)
-        {
-            List<int> NewArray = new List<int>();
-            int test_case;int missing_Element=0;
-            Console.WriteLine("Please enter the number of test cases");
-            test_case = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < test_case; i++)
-            {
-                Console.WriteLine("Please enter the size of the array");
-                int n = Convert.ToInt32(Console.ReadLine());
-                int[] A = new int[n];
-                Console.WriteLine("Please ente the elements");
-                for (int j = 0; j < n - 1; j++)
-                {
-                    A[j] = Convert.ToInt32(Console.ReadLine());
-                    NewArray.Add(A[j]);
-                }
-                for (int k = 1; k < n; k++)
-                {
-                    foreach (var v in A)
-                        if (k == v)
-                        {
-                            NewArray.Remove(v);
-                            A = NewArray.ToArray();
-                            break;
-                        }
-                        else
-                            missing_Element = k;
-                }
-                Console.WriteLine("Missing Element in the array is {0}", missing_Element);
+//        public static void Main(string[] args)
+//        {
+//            List<int> NewArray = new List<int>();
+//            int test_case;int missing_Element=0;
+//            Console.WriteLine("Please enter the number of test cases");
+//            test_case = Convert.ToInt32(Console.ReadLine());
+//            for (int i = 0; i < test_case; i++)
+//            {
+//                Console.WriteLine("Please enter the size of the array");
+//                int n = Convert.ToInt32(Console.ReadLine());
+//                int[] A = new int[n];
+//                Console.WriteLine("Please ente the elements");
+//                for (int j = 0; j < n - 1; j++)
+//                {
+//                    A[j] = Convert.ToInt32(Console.ReadLine());
+//                    NewArray.Add(A[j]);
+//                }
+//                for (int k = 1; k < n; k++)
+//                {
+//                    foreach (var v in A)
+//                        if (k == v)
+//                        {
+//                            NewArray.Remove(v);
+//                            A = NewArray.ToArray();
+//                            break;
+//                        }
+//                        else
+//                            missing_Element = k;
+//                }
+//                Console.WriteLine("Missing Element in the array is {0}", missing_Element);
 
-            }
+//            }
             
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ SubArrayWithGivenSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class SubArrayWithGivenSum
+    {
+        public static void Main(string[] args)
+        {
+            int test_case; int size=0; int sum;int elementsum=0;int first=0;int last=0;
+            Console.WriteLine("Please enter the number test cases");
+            test_case = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < test_case; i++)
+            {
+                Console.WriteLine("Please enter the size and sum for array");
+                size = Convert.ToInt32(Console.ReadLine());
+                sum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the array elemennts");
+                int[] A = new int[size];
+                for (int j = 0; j < size; j++)
+                    A[j] = Convert.ToInt32(Console.ReadLine());
+                for (int k = 0; k < size; k++)
+                {
+                    elementsum = A[k];
+                    for(int m=k+1;m<size;m++)
+                    {
+                             elementsum += A[m];
+                             if (sum == elementsum)
+                             {
+                                 first = k;
+                                 last = m;
+                                 Console.WriteLine("Sum of element from  " + first + " position to  " + last + " position is " + sum + "");
+                                 break;
+                             }
+                    }
+                    elementsum = 0;
+                }
+                
+            }
+
+
+        }
+    }
+}
