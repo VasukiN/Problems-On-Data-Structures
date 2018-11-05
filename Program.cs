0a8d7e0ed7081b57b7d597c52e170b914c1354c3
VasukiN <vasuki.hebbar@gmail.com>
2018-11-05 20:01:51 +0530

    Problems on Array Rotation and string length
    Problems on Array Rotation and string length

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringlengthProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            
+            for (int i = 0; str[i]!="\n";i++ )
+            
+                count++;
+                //Console.WriteLine("Number of characters", +count);
+                Console.ReadLine();
+                //count = i;
+            }
+            
+            
+        }
+    }
+}
